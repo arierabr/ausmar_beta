4e9476d01ee39e19b8c9ba1c7003d5475cc192f5
arierabr <rierabrellalfons@gmail.com>
2024-10-15 13:03:14 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -192,8 +192,8 @@ if st.button("Predict"):
             datos_plot_ref = datos_plot[datos_plot["Producto"] == reference].groupby('Semana')['Cantidad'].sum().reset_index()
             datos_plot_ref.set_index(['Semana'], inplace=True)
             datos_plot_ref.index = pd.to_datetime(datos_plot_ref.index)
-            tr = datos_plot_ref.iloc[:-40]
-            tst = datos_plot_ref.iloc[-41:]
+            tr = datos_plot_ref.iloc[:-10]
+            tst = datos_plot_ref.iloc[-11:]
 
 
 
